2529c8380bf31ef742dc466685b99b825ba63041
Veronika ArtIvanika <petrovskaya.veronika@gmail.com>
2023-08-19 22:40:04 +0300

    изменила ограничения пароля до 8 символов

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -6,7 +6,7 @@ export const routesWithFooter = ["/", "/movies", "/saved-movies"];
 export const SCREEN_MD = 768;
 
 export const nameRegex = /^[A-Za-zА-Яа-я -]{2,}$/;
-export const passwordRegex = /^[\w\W]{6,}$/;
+export const passwordRegex = /^[\w\W]{8,}$/;
 
 
 export const RESIZE_TIMEOUT = 800;
